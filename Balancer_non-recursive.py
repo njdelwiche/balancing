2bcf8d5a4d31d346280f974c75fe313ec1b57a20
root <root@DELL-IH4MG7EQ4P.localdomain>
2020-02-25 20:09:01 -0500

    removed comments

--- Balancer_non-recursive.py
+++ Balancer_non-recursive.py
@@ -1,23 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Balancing Groups
-# 
-# This program helps create balanced sub-groups from a larger roster. After loading in a "roster.csv" file, the user can edit the group sizes and criteria along which they are balancing. <p>
-# 
-# The program first examines the overall distribution of values across the balance criteria and sets a ceiling for the number each group can contain (e.g. 15 males). <p>
-# 
-# It then balances the groups by semi-randomly swapping items. It ensures not to backtrack through swaps by:
-# 
-#     1. Creating the first sub-group by extracting a sample from the larger roster
-#     2. Checking the balance of that first sub-group
-#     3. Swapping items in the first sub-group with only items from the larger roster
-#     4. Once the first group is finished, it breaks another sub-group from the larger roster 
-#     5. It then swaps items in the second-group with only the larger (yet now slightly diminished) roster
-#     , leaving the first group untouched
-#     6. It continues until all groups are balanced 
-# 
-# It then ensures that the swaps have not changed the overall, groups-total distribution of values. Finally, it outputs a csv file with a new column that indicates the balanced group pairings.
 
 import pandas as pd
 import numpy as np
